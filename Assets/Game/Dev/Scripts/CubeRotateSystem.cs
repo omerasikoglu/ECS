76c15c1d635301e0ef737676a3223978f0ebedcd
Ömer Aşıkoğlu <faruk.asikoglu@gmail.com>
2024-09-28 23:02:26 +0300

    Update

---
+++ Assets/Game/Dev/Scripts/CubeRotateSystem.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public partial struct CubeRotateSystem : ISystem{
+
+  public void OnUpdate(ref SystemState state){
+    foreach (var (localTrnasform, rotateSpeed) in 
+             SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>>()){
+      localTrnasform.ValueRW = localTrnasform.ValueRO.
+        RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime);
+    }
+  }
+}
---
+++ Assets/Game/Dev/Scripts/RotateSpeedAuthoring.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+public struct RotateSpeed : IComponentData{
+  public float value;
+}
+
+public class RotateSpeedAuthoring : MonoBehaviour{
+
+  public float value;
+
+  class DummyBaker : Baker<RotateSpeedAuthoring>{
+
+    public override void Bake(RotateSpeedAuthoring authoring){
+      var entity = GetEntity(TransformUsageFlags.Dynamic);
+      AddComponent(entity, new RotateSpeed { value = authoring.value });
+    }
+  }
+
+}
